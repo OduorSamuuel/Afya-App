e57ff6271665fabf1d8f9e36745a9f432eb87a2d
oduorsamuuel <onyangos949@gmail.com>
2026-07-07 13:06:10 +0300

    feat: configure Entity Framework Core database connection
    - Add EF Core connection string for SQL Server

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AfyaApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AfyaApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
