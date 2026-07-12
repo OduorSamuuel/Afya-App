1e6ea77f4ab141f86a89052990b03789c3623ebf
oduorsamuuel <onyangos949@gmail.com>
2026-07-12 08:46:50 +0300

    feat:Add patient domain

--- Domain/Patient.cs
+++ Domain/Patient.cs
@@ -1,6 +1,17 @@
-﻿namespace AfyaApp.Domain
+﻿using Ardalis.SharedKernel;
+
+namespace AfyaApp.Domain
 {
-    public class Patient
+    public class Patient:IAggregateRoot
     {
+        public Guid Id { get; private set; }
+        public string FullName { get; private set; } = string.Empty;
+        public DateTime DateOfBirth { get; private set; }
+        public string Gender { get; private set; } = string.Empty;
+        public string PhoneNumber { get; private set; } = string.Empty;
+
+        public int RegisteredById { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
     }
 }
