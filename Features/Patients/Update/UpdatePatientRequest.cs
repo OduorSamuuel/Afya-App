b8a1dda438791e726f34622375083ae60f96c069
oduorsamuuel <onyangos949@gmail.com>
2026-07-17 03:28:28 +0300

    feat:Add update endpoint

---
+++ Features/Patients/Update/UpdatePatientRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace AfyaApp.Features.Patients.Update;
+
+public class UpdatePatientRequest
+{
+    public const string Route = "/patient/{Id}";
+    public Guid Id { get; init; }
+    public string FullName { get; init; } = string.Empty;
+    public DateTime DateOfBirth { get; init; }
+    public string Gender { get; init; } = string.Empty;
+    public string PhoneNumber { get; init; } = string.Empty;
+}
